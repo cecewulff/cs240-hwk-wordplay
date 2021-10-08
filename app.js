79c9347bb78dbc8d37faf99a4a6179a054e4c267
Cecelia Wulff <clwulff@pugetsound.edu>
2021-10-07 18:33:14 -0700

    methods for finding random 6 letter root words

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 //function to find root word with arrow notation and "filter"
 function sixLetters(dictionary) {
-    return dictionary.filter(x => (x.length() < 7));
-
-function startingWord(sixLetters) {
-
+  let rootWords = dictionary.filter((x) => x.length() == 6);
 }
-//function to randomly select a word from the array of 6 letter words  
-function randomWord(min, max) {
-    return Math.floor(Math.random() * sixLetters.length());
-
+//function to randomly select a word from the array of 6 letter words
+function randomWord(rootWords) {
+  let wordNumber = Math.floor(Math.random() * rootWords.length());
+  console.log(rootWords[wordNumber]);
 }
 
 //first attempt at same function with forEach
 //function sixLetters(dictionary) {
-  //let root = [];
-  //dictionary.forEach((x) => {
-    //if (x.length() == 6) root.push(x);
-  //});
-  //return root();
+//let root = [];
+//dictionary.forEach((x) => {
+//if (x.length() == 6) root.push(x);
+//});
+//return root();
 //}
-
-
-
