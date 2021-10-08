47d4f73a7fb3b65a38e3ef4213b957dcebc560ce
Cecelia Wulff <clwulff@pugetsound.edu>
2021-10-07 17:03:55 -0700

    function to find 6 letter word added

---
+++ app.js
@@ -0,0 +1,19 @@
+//function to find root word with arrow notation and "filter"
+function sixLetters(dictionary) {
+    return dictionary.filter(x => (x.length() < 7));
+
+function startingWord(sixLetters) {
+    
+}
+    
+//first attempt at same function with forEach
+//function sixLetters(dictionary) {
+  //let root = [];
+  //dictionary.forEach((x) => {
+    //if (x.length() == 6) root.push(x);
+  //});
+  //return root();
+//}
+
+
+
