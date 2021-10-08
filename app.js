47cd9b868d49c1c3ce5a03320aeb8acaa551846a
Cecelia Wulff <clwulff@pugetsound.edu>
2021-10-07 18:15:30 -0700

    added function to randomly select a word from array of 6 letter words

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ function sixLetters(dictionary) {
     return dictionary.filter(x => (x.length() < 7));
 
 function startingWord(sixLetters) {
-    
+
 }
-    
+//function to randomly select a word from the array of 6 letter words  
+function randomWord(min, max) {
+    return Math.floor(Math.random() * sixLetters.length());
+
+}
+
 //first attempt at same function with forEach
 //function sixLetters(dictionary) {
   //let root = [];
