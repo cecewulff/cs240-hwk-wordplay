e7e91861fa7f029bcf6e095455debb5f9e82bab8
Cecelia Wulff <clwulff@pugetsound.edu>
2021-10-08 17:16:45 -0700

    Added a method to gather all the letters in the mystery word into an array

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ function sixLetters(dictionary) {
 //function to randomly select a word from the array of 6 letter words
 function randomWord(rootWords) {
   let wordNumber = Math.floor(Math.random() * rootWords.length()); // finding random place in array
-  console.log(rootWords[wordNumber]); //printing string at that random place
+  let mysteryWord = rootWords[wordNumber];
+}
+
+//function to find root word with arrow notation and "filter"
+//function to randomly select a word from the array of 6 letter words
+
+function randomWord(rootWords) {
+  function sixLetters(dictionary) {
+    let rootWords = dictionary.filter((x) => x.length() == 6); //array with 6 letter words
+    let wordNumber = Math.floor(Math.random() * rootWords.length()); // finding random place in array
+    let mysteryWord = rootWords[wordNumber]; // finding word at that random place in array of 6 letter words
+    //console.log(rootWords['wordNumber']); //printing string at that random place
+  }
+}
+
+//i want to add the letters of the mystery word to an array so they are more easily distinguisable
+//function to loop through the mystery word and add each letter to an array the "old way"
+function letters(mysteryWord) {
+  let letter = [];
+  for (i = 0; i < 7; i++) {
+    letter.push(substring(i));
+  }
+  return letter;
 }
 
 //first attempt at same function with forEach
@@ -15,4 +37,3 @@ function randomWord(rootWords) {
 //if (x.length() == 6) root.push(x);
 //});
 //return root();
-//}
